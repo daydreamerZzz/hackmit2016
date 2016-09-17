835a52c8b20a200a352e0503e878f59c8578852b
Sarah Sukardi <sukardi@jhu.edu>
2016-09-17 19:48:21 -0400

    removed unnecessary emailing code

--- database/index.js
+++ database/index.js
@@ -18,16 +18,9 @@
 // [START imports]
 var firebase = require('firebase');
 // [END imports]
-var nodemailer = require('nodemailer');
 var schedule = require('node-schedule');
 var Promise = require('promise');
 var escape = require('escape-html');
-
-// TODO(DEVELOPER): Configure your email transport.
-// Configure the email transport using the default SMTP transport and a GMail account.
-// See: https://nodemailer.com/
-// For other types of transports (Amazon SES, Sendgrid...) see https://nodemailer.com/2-0-0-beta/setup-transporter/
-var mailTransport = nodemailer.createTransport('smtps://voicelessalveolarsibilant%40gmail.com:linguistics@smtp.gmail.com');
 
 // TODO(DEVELOPER): Change the two placeholders below.
 // [START initialize]
@@ -49,39 +42,15 @@ function sendNotificationToUser(uid, postId) {
     var email = snapshot.val().email;
     // Send the email to the user.
     // [START_EXCLUDE]
-    if (email) {
-      sendNotificationEmail(email).then(function() {
-        // Save the date at which we sent that notification.
-        // [START write_fan_out]
-        var update = {};
-        update['/posts/' + postId + '/lastNotificationTimestamp'] =
-            firebase.database.ServerValue.TIMESTAMP;
-        update['/user-posts/' + uid + '/' + postId + '/lastNotificationTimestamp'] =
-            firebase.database.ServerValue.TIMESTAMP;
-        firebase.database().ref().update(update);
-        // [END write_fan_out]
-      });
-    }
+    var update = {};
+    update['/posts/' + postId + '/lastNotificationTimestamp'] =
+        firebase.database.ServerValue.TIMESTAMP;
+    update['/user-posts/' + uid + '/' + postId + '/lastNotificationTimestamp'] =
+        firebase.database.ServerValue.TIMESTAMP;
+    firebase.database().ref().update(update);    
     // [END_EXCLUDE]
   }).catch(function(error) {
     console.log('Failed to send notification to user:', error);
-  });
-}
-// [END single_value_read]
-
-
-/**
- * Send the new star notification email to the given email.
- */
-function sendNotificationEmail(email) {
-  var mailOptions = {
-    from: '"Firebase Database Quickstart" <noreply@firebase.com>',
-    to: email,
-    subject: 'New star!',
-    text: 'One of your posts has received a new star!'
-  };
-  return mailTransport.sendMail(mailOptions).then(function() {
-    console.log('New star email notification sent to: ' + email);
   });
 }
 
@@ -131,69 +100,5 @@ function startListeners() {
   console.log('Likes count updater started...');
 }
 
-/**
- * Send an email listing the top posts every Sunday.
- */
-function startWeeklyTopPostEmailer() {
-  // Run this job every Sunday at 2:30pm.
-  schedule.scheduleJob({hour: 14, minute: 30, dayOfWeek: 0}, function () {
-    // List the top 5 posts.
-    // [START top_posts_query]
-    var topPostsRef = firebase.database().ref('/posts').orderByChild('starCount').limitToLast(5);
-    // [END top_posts_query]
-    var allUserRef = firebase.database().ref('/users');
-    Promise.all([topPostsRef.once('value'), allUserRef.once('value')]).then(function(resp) {
-      var topPosts = resp[0].val();
-      var allUsers = resp[1].val();
-      var emailText = createWeeklyTopPostsEmailHtml(topPosts);
-      sendWeeklyTopPostEmail(allUsers, emailText);
-    }).catch(function(error) {
-      console.log('Failed to start weekly top posts emailer:', error);
-    });
-  });
-  console.log('Weekly top posts emailer started...');
-}
-
-/**
- * Sends the weekly top post email to all users in the given `users` object.
- */
-function sendWeeklyTopPostEmail(users, emailHtml) {
-  Object.keys(users).forEach(function(uid) {
-    var user = users[uid];
-    if (user.email) {
-      var mailOptions = {
-        from: '"Firebase Database Quickstart" <noreply@firebase.com>',
-        to: user.email,
-        subject: 'This week\'s top posts!',
-        html: emailHtml
-      };
-      mailTransport.sendMail(mailOptions).then(function() {
-        console.log('Weekly top posts email sent to: ' + user.email);
-        // Save the date at which we sent the weekly email.
-        // [START basic_write]
-        return firebase.database().child('/users/' + uid + '/lastSentWeeklyTimestamp')
-            .set(firebase.database.ServerValue.TIMESTAMP);
-        // [END basic_write]
-      }).catch(function(error) {
-        console.log('Failed to send weekly top posts email:', error);
-      });
-    }
-  });
-}
-
-/**
- * Creates the text for the weekly top posts email given an Object of top posts.
- */
-function createWeeklyTopPostsEmailHtml(topPosts) {
-  var emailHtml = '<h1>Here are this week\'s top posts:</h1>';
-  Object.keys(topPosts).forEach(function(postId) {
-    var post = topPosts[postId];
-    emailHtml += '<h2>' + escape(post.title) + '</h2><div>Author: ' + escape(post.author) +
-        '</div><div>Stars: ' + escape(post.starCount) + '</div><p>' + escape(post.body) + '</p>';
-  });
-  return emailHtml;
-}
-
 // Start the server.
 startListeners();
-startWeeklyTopPostEmailer();
